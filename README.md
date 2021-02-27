93cfa60e741f99be72cbf949b93afb21a9500e8a
Yuta Okafuji <okafuji@kdl.co.jp>
2021-02-28 00:49:43 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ofuji-works.github.io
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react'
+
+const Index: FC = () => {
+  return <h1>Hello World</h1>
+}
+
+export default Index
