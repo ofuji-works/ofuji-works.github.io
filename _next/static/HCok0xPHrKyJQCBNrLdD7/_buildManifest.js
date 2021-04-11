99132583dbedf4980ee3d24ee73a2e5e431e47be
ofuji-works <ofuji-works@users.noreply.github.com>
2021-04-11 08:13:36 +0000

    deploy: 5a6e209ac63dc58515fc5a15d5de909b3b8d3eba

--- _next/static/HCok0xPHrKyJQCBNrLdD7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-9dbf26989f7299aa3665.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-65d0fdb5c028d0cabe7e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
